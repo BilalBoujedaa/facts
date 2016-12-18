17c1428a5b2dd73bd19fcaa21f0bfa0b8096595e
AbdullahAlh <young.moby@gmail.com>
2016-12-18 12:05:07 +0300

    first-version

---
+++ app/src/main/java/fugoki/com/recyclingfacts/RecyclingFactsActivity.java
@@ -0,0 +1,13 @@
+package fugoki.com.recyclingfacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecyclingFactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycling_facts);
+    }
+}
