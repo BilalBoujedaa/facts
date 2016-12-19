00603e816070139995ec93940eff21e40163c9a2
AbdullahAlh <young.moby@gmail.com>
2016-12-19 16:28:27 +0300

    Code refactoring

--- app/src/main/java/fugoki/com/recyclingfacts/RecyclingFactsActivity.java
+++ app/src/main/java/fugoki/com/recyclingfacts/RecyclingFactsActivity.java
@@ -14,19 +14,7 @@ public class RecyclingFactsActivity extends AppCompatActivity {
     private TextView factTextView;
     private Button factButton;
 
-    String facts[] = {
-            "95% من معلومات العالم لاتزال مخزنه على الورق واغلبها لاتتم مشاهدته مرة اخرى",
-            "اعادة تدوير قاروة بلاستيك يوفر طاقة كافية لاضاءة مصباح بقدرة 60 واط لمدة 6 ساعات",
-            "الاف المخلوقات البحرية تموت بسبب ابتلاعها اكياس البلاستيك التي تشبه قنديل البحر",
-            "في كل عام يرمي الامريكان قوارير وزجاجات كافية لوصول القمر والعودة اليه 20 مره",
-            "اعادة تدوير طن من البلاستيك يمكن ان يوفر حوالي 2000 غالون من البنزين",
-            "بداية من عام 2015 جميع سكان نيويورك عليهم اعادة تدوير الاجهزة الالكترونية والا سيواجهون غرامه مقدارها 100 دولا",
-            "اعادة تدوير علبة المنيوم يمكن ان يوفر طاقة تكفي لتشغيل تلفاز لساعتين",
-            "اعادة تدوير طن من الورق يحافظ على 17 شجره",
-            "اعادة تدوير علبة المنيوم يمكن ان يعيدها الى رف متجر خلال 60 يوم",
-
-            "إبدأ إعادة التدوير بتقليل استخدامك للبلاستك"
-    };
+    private RecyclingFactsGenerator recyclingFactsGenerator = new RecyclingFactsGenerator();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +29,7 @@ public class RecyclingFactsActivity extends AppCompatActivity {
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // هنا سيتم تغير الجملة في البرنامج
-                String fact;
-                Random randomGen = new Random();
-                int randomNumber = randomGen.nextInt(facts.length);
-
-                fact = facts[randomNumber];
+                String fact = recyclingFactsGenerator.generateFact();
 
                 factTextView.setText(fact);
             }
