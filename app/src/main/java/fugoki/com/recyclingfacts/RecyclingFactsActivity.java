1c0163efe7c42e32ebf147fe5dffab0986a1ae34
AbdullahAlh <young.moby@gmail.com>
2016-12-19 14:04:07 +0300

    Array container added

--- app/src/main/java/fugoki/com/recyclingfacts/RecyclingFactsActivity.java
+++ app/src/main/java/fugoki/com/recyclingfacts/RecyclingFactsActivity.java
@@ -14,6 +14,18 @@ public class RecyclingFactsActivity extends AppCompatActivity {
     private TextView factTextView;
     private Button factButton;
 
+    String facts[] = {
+            "95% من معلومات العالم لاتزال مخزنه على الورق واغلبها لاتتم مشاهدته مرة اخرى",
+            "اعادة تدوير قاروة بلاستيك يوفر طاقة كافية لاضاءة مصباح بقدرة 60 واط لمدة 6 ساعات",
+            "الاف المخلوقات البحرية تموت بسبب ابتلاعها اكياس البلاستيك التي تشبه قنديل البحر",
+            "في كل عام يرمي الامريكان قوارير وزجاجات كافية لوصول القمر والعودة اليه 20 مره",
+            "اعادة تدوير طن من البلاستيك يمكن ان يوفر حوالي 2000 غالون من البنزين",
+            "بداية من عام 2015 جميع سكان نيويورك عليهم اعادة تدوير الاجهزة الالكترونية والا سيواجهون غرامه مقدارها 100 دولا",
+            "اعادة تدوير علبة المنيوم يمكن ان يوفر طاقة تكفي لتشغيل تلفاز لساعتين",
+            "اعادة تدوير طن من الورق يحافظ على 17 شجره",
+            "اعادة تدوير علبة المنيوم يمكن ان يعيدها الى رف متجر خلال 60 يوم"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,11 +40,13 @@ public class RecyclingFactsActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // هنا سيتم تغير الجملة في البرنامج
-                String fact = "في كل عام يرمي الامريكان قوارير وزجاجات كافية لوصول القمر والعودة اليه 20 مره";
+                String fact;
                 Random randomGen = new Random();
                 int randomNumber = randomGen.nextInt(9);
 
-                factTextView.setText(randomNumber + "");
+                fact = facts[randomNumber];
+
+                factTextView.setText(fact);
             }
         };
 
